ae0cefd2afc45398b14a9cfa869f0a41641e0ff8
nolwazi749 <nolwazizwane7@gmail.com>
2020-11-03 06:01:11 -0500

    Listening to the port

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,4 @@ const app = express()
 app.use(express.json())
 
 const port = 4000
-app.listen(port, ()=> console.log(`server running on port ${port}`))
+app.listen(port, ()=> console.log(`server running on port: ${port}`))
