dfdb6c0116e11fd9530b722028d35361ca507ddd
nolwazi749 <nolwazizwane7@gmail.com>
2020-11-04 02:10:01 -0500

    i connected server to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,24 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
 app.use(express.json())
+mongoose.connect(
+  "mongodb+srv://kwanele:<password>@cluster0.lt0xm.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    {
+userNewUrlParser: true,
+useUnifiedTopology: true,
+useNewUrlParser: true
+    },
 
-const port = 4000
+    () =>{
+        console.log("mongobd database connected!")
+    }
+);
+
+const customerRouter = require('./route/customer')
+app.use("/customer", customerRouter)
+
+
+const port = 5100
 app.listen(port, ()=> console.log(`server running on port: ${port}`))
