dc07059b90662fc626b002aa809d660723087504
nolwazi749 <nolwazizwane7@gmail.com>
2020-11-06 02:49:51 -0500

    mongoose

--- backend/route/customer.js
+++ backend/route/customer.js
@@ -1,30 +1,31 @@
-const router = require('express').Router();
 const Customer = require('../model/customer')
 
-router.post("/book", async (req, res) => {
-    const {name, surname, email, dietary} = req.body
+const route = (app) => {
+  app.post("/book", async (req, res) => {
+    console.log("anything")
+    const { name, surname, email, dietary } = req.body
     const customer = new Customer({
-       name: name,
-       surname: surname, 
-       email: email,
-       dietary: dietary
+      name: name,
+      surname: surname,
+      email: email,
+      dietary: dietary
     })
-
     try {
-        const savedCustomer = await customer.save();
-        res.send(savedCustomer);
-    } catch (error) {
-        res.send(error);
-    }
-})
-
-router.get("/", async (req, res) => {
-    try {
-      const customer = await Customer.find();
-      res.send(customer);
+      const savedCustomer = await customer.save();
+      res.send(savedCustomer);
     } catch (error) {
       res.send(error);
     }
-  });
+  })
+}
 
-module.exports = router
+// router.get("/", async (req, res) => {
+//     try {
+//       const customer = await Customer.find();
+//       res.send(customer);
+//     } catch (error) {
+//       res.send(error);
+//     }
+//   });
+
+module.exports = { route }
