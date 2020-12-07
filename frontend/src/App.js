1fcafb5da714ef49c88cf3a9bf0ac7f06e2c93a6
nolwazi749 <nolwazizwane7@gmail.com>
2020-12-07 04:17:50 -0500

    added router and route on my App.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 
 import './App.css';
 import Events from './component/Events'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import NameForm from './component/Forms';
 function App() {
   return (
     <div className="App">
-      <Events/>
-      
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Events} />
+          <Route exact path="/book" component={NameForm} />
+        </Switch>
+      </Router>
+
     </div>
   );
 }
